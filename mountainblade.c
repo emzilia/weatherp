f7c9bc7404a1b8488dc602a32214015aa1a84da6
emzilia <giblinhm@miamioh.edu>
2023-06-20 11:23:27 -0400

    updated enter city action WIP

--- mountainblade.c
+++ mountainblade.c
@@ -2,115 +2,140 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <math.h>
 #include <ncurses.h>
 
-int display(char *text);
+//int display(char *text);
 void look();
 void city_all();
 void print_userinfo();
+
+void action_enter_city();
+void action_contact_noble();
+void action_hire_mercs();
+void action_view_inventory();
+void action_view_relations();
+void action_view_quests();
 
 typedef struct {
 	char name[6]; 
 	size_t army;
 	size_t charisma;
 	int denars;
-	char title[6];
-	char location[14];
+	char title[8];
+	char location[20];
 	int intown;
-	size_t x;
-	size_t y;
+	int x;
+	int y;
+	int xy;
 } User;
 
 typedef struct {
 	char name[7];
-	char location[15];
-	char noble[15];
+	char location[20];
+	char noble[20];
 	size_t pop;
 	size_t danger;
 	size_t wealth;
-	size_t x;
-	size_t y;
+	int x;
+	int y;
+	int xy;
 } City;
+
+User p = {
+	.name = "Elena",
+	.army = 40,
+	.charisma = 5,
+	.denars = 120,
+	.title = "Esquire",
+	.x = 1,
+	.y = 1,
+};
 
 const City zander = {
 	.name = "Zander",
 	.location = "Town of Zander",
-	.noble = "Lord Balder",
-	.pop = 22300,
+	.noble = "Baron Balder",
+	.pop = 3300,
 	.danger = 5,
 	.wealth = 3,
 	.x = 2,
 	.y = 2,
+	.xy = 2 + 2,
 };
 
 const City talis = {
 	.name = "Talis",
 	.location = "City of Talis",
-	.noble = "Lady Alinna",
-	.pop = 27400,
+	.noble = "Marquise Alinna",
+	.pop = 7430,
 	.danger = 3,
 	.wealth = 7,
 	.x = 3,
 	.y = 6,
+	.xy = 3 + 6,
 };
 
 const City adriin = {
 	.name = "Adriin",
-	.location = "City of Adriin",
-	.noble = "Lady Merisa",
-	.pop = 38500,
+	.location = "Castle of Adrin",
+	.noble = "The Viscountess",
+	.pop = 11570,
 	.danger = 1,
 	.wealth = 5,
 	.x = 6,
 	.y = 4,
+	.xy = 6 + 4,
 };
 
 const City doxoun = {
 	.name = "Doxoun",
 	.location = "City of Doxoun",
-	.noble = "Lady Raegai",
-	.pop = 11600,
+	.noble = "Baroness Raegai",
+	.pop = 5620,
 	.danger = 7,
 	.wealth = 9,
 	.x = 6,
 	.y = 1,
+	.xy = 6 + 1,
 };
 
 const City calia = {
 	.name = "Calia",
 	.location = "Town of Calia",
-	.noble = "Lord Kieran",
+	.noble = "Sir Kieran",
 	.pop = 2350,
 	.danger = 2,
 	.wealth = 2,
 	.x = 9,
 	.y = 5,
+	.xy = 9 + 5,
 };
 
 const City grelin = {
 	.name = "Grelin",
 	.location = "Town of Grelin",
-	.noble = "Lady Alinna",
+	.noble = "Marquise Alinna",
 	.pop = 870,
 	.danger = 4,
 	.wealth = 1,
 	.x = 11,
 	.y = 3,
+	.xy = 11 + 3,
 };
 
+void update_pxy()
+{
+	char px[2];
+	char py[2];
+	char pxy[5];
 
-User p = {
-	.name = '@',
-	.army = 40,
-	.charisma = 5,
-	.denars = 120,
-	.title = "Knight",
-	.x = 1,
-	.y = 1,
-};
-
-int posX = 1;
-int posY = 1;
+	sprintf(px, "%d", p.x);
+	sprintf(px, "%d", p.x);
+	
+//	pxy = strcat(px, py);
+	p.xy = atoi(pxy);
+}
 
 char map[8][14] = {
 	"o============o",
@@ -129,19 +154,19 @@ int columns = sizeof(map[0]) / sizeof(map[0][0]);
 
 void title_screen()
 {
-	printw("Hello and welcome to Tania!");
+	printw(
+		"Hello and welcome to Tania!\n\n"
+		"The Duke has been gone for three years now,\n"
+		"fighting heathens abroad. Unrest grows across the\n"
+		"realm as local nobles centralize their power."
+	);
 
 	getch();
 	clear();
 }
 
-void user_display()
-{
-	
-}
-
 void print_map(char arr[][14], int rows, int columns) {
-	printw("Realm of Tania\n");
+	printw("Dutchy of Tania\n");
 	for (int r = 0; r < rows; r++) {
 		for (int c = 0; c < columns; c++) {
 			if (r == p.y && c == p.x)
@@ -150,7 +175,7 @@ void print_map(char arr[][14], int rows, int columns) {
 				printw("%c", arr[r][c]);
 		}
 	addch('\n'); }
-	//printw(" %zu, %zu\n", p.x, p.y);
+	//printw(" %d, %d\n%d\n", p.x, p.y, p.xy);
 	city_all();
 }
 
@@ -165,6 +190,7 @@ int city_look(City town)
 {
 	if (p.x == town.x && p.y == town.y) {
 		p.intown = 1;
+		strcpy(p.location, town.location);
 		printw("%s\nPopulation of %zu\n\n", town.location, town.pop);
 		return 1;
 	}
@@ -215,9 +241,41 @@ void print_actions()
 void action_enter_city()
 {
 	clear();
-	printw("You approach the settlement gates and present\nyourself to the guard. He ushers your party\nthrough without complaint.");
+	printw(
+		"You approach the gates to the %s\nThe guard ushers"
+		" your party through without complaint.",
+		p.location);
 	refresh();
 	getch();
+
+	int cityloop = 1;
+	while (cityloop) {
+		clear();
+		printw(
+			"%s\n\nWho would you like to see?\n\n"
+			"1. Guild Master\n2. Blacksmith\n3. Stablekeeper",
+			p.location
+		);	
+		refresh();
+		int response = getch();
+		switch (response) {
+			case '1':
+				clear();
+				printw(
+					"Guild Master:\n\n Hello %s, "
+					"how can we help you today?",
+					p.name
+				);
+				refresh();
+				break;
+			case '2':
+				action_contact_noble();
+				break;
+			case 'q':
+				cityloop = 0;
+				break;
+		}
+	}
 }
 
 void action_contact_noble()
@@ -236,16 +294,32 @@ void action_hire_mercs()
 	getch();
 }
 
+void action_view_inventory()
+{
+	clear();
+	printw("Notable trinkets:\n\nYour father's sword\nGold engraved locket");
+	refresh();
+	getch();
+}
+
 void action_view_relations()
 {
 	clear();
-	printw("%s\n%s\n\n%s\n%s, %s\n\n%s\n%s\n\n%s\n%s\n\n%s\n%s\n",
+	printw("Relations:\n\n%s\n%s\n\n%s\n%s, %s\n\n%s\n%s\n\n%s\n%s\n\n%s\n%s\n",
 		zander.noble, zander.location,
 		talis.noble, talis.location, grelin.location,
 		adriin.noble, adriin.location,
 		doxoun.noble, doxoun.location,
 		calia.noble, calia.location
 	);
+	refresh();
+	getch();
+}
+
+void action_view_quests()
+{
+	clear();
+	printw("Quests:\n\nDeliver letter to %s\n- For %s", zander.noble, adriin.noble);
 	refresh();
 	getch();
 }
@@ -258,21 +332,25 @@ void movement()
 		case KEY_UP:
 			if (p.y > 1)
 				p.y--;
+				//update_pxy();
 			break;
 		case 'j':
 		case KEY_DOWN:
 			if (p.y < rows - 2)
 				p.y++; 
+				//update_pxy();
 			break;
 		case 'h':
 		case KEY_LEFT:
 			if (p.x > 1)
 				p.x--;
+				//update_pxy();
 			break;
 		case 'l':
 		case KEY_RIGHT:
 			if (p.x < columns - 2)
 				p.x++;
+				//update_pxy();
 			break;
 	}
 	
@@ -301,13 +379,13 @@ void movement()
 	
 	switch (response) {
 		case 'i':
-			//action_view_inventory();
+			action_view_inventory();
 			break;
 		case 'r':
 			action_view_relations();
 			break;
 		case 'q':
-			//action_view_quests();
+			action_view_quests();
 			break;
 	}
 }
