3e33d80aeaa27a7f4a3f9326c4f114799759d0cb
emzilia <giblinhm@miamioh.edu>
2023-06-21 15:24:24 -0400

    updated actions to use new window

--- actions.c
+++ actions.c
@@ -3,28 +3,29 @@
 
 #include "entities.h"
 #include "actions.h"
+#include "display.h"
 
 void action_enter_city()
 {
-	clear();
-	printw(
+	wclear(win);
+	wprintw(win,
 		"You approach the gates to the %s\nThe guard ushers"
 		" your party through without complaint.",
 		p.location);
-	refresh();
+	wrefresh(win);
 	getch();
 
 	int cityloop = 1;
 	while (cityloop) {
-		clear();
-		printw(
+		wclear(win);
+		wprintw(win,
 			"%s\n\nWho would you like to see?\n\n"
 			"1. Guild Master\n2. Tailor\n"
 			"3. Blacksmith\n4. Stablekeeper\n"
 			"\n(b) to go back",
 			p.location
 		);	
-		refresh();
+		wrefresh(win);
 		int response = getch();
 		switch (response) {
 			case '1':
@@ -48,110 +49,107 @@ void action_enter_city()
 
 void action_enter_city_guildmaster()
 {
-	clear();
-	printw(
+	wclear(win);
+	wprintw(win,
 		"Guild Master:      ($%d in your inventory)\n\nHello %s, "
 		"how can we help you today?\n",
 		p.denars, p.name
 	);
-	refresh();
+	wrefresh(win);
 	getch();
 }
 
 void action_enter_city_tailor()
 {
-	clear();
-	printw(
+	wclear(win);
+	wprintw(win,
 		"Tailor:\n\nHello %s, "
 		"how can we help you today?",
 		p.name
 	);
-	refresh();
+	wrefresh(win);
 	getch();
 }
 
 void action_enter_city_blacksmith()
 {
-	clear();
-	printw(
+	wclear(win);
+	wprintw(win,
 	"Blacksmith:\n\nHello %s, "
 	"how can we help you today?",
 	p.name
 	);
-	refresh();
+	wrefresh(win);
 	getch();
 }
 
 void action_enter_city_stablekeeper()
 {
-	clear();
-	printw(
+	wclear(win);
+	wprintw(win,
 	"Stablekeeper:\n\nHello %s, "
 	"how can we help you today?",
 	p.name
 	);
-	refresh();
+	wrefresh(win);
 	getch();
 }
 
 void action_contact_noble()
 {
-	clear();
-	printw("You send your aid to the estate of the local\nnoble, hoping to arrange a meeting.");
-	refresh();
+	wclear(win);
+	wprintw(win, "You send your aid to the estate of the local\nnoble, hoping to arrange a meeting.");
+	wrefresh(win);
 	getch();
 }
 
 void action_hire_mercs()
 {
-	clear();
-	printw("You send messengers to the local taverns,\nhoping to find some troops to hire.");
-	refresh();
+	wclear(win);
+	wprintw(win, "You send messengers to the local taverns,\nhoping to find some troops to hire.");
+	wrefresh(win);
 	getch();
 }
 
 void action_setup_camp()
 {
-	clear();
-	printw("You send messengers to the local taverns,\nhoping to find some troops to hire.");
-	refresh();
+	wclear(win);
+	wprintw(win, "You send messengers to the local taverns,\nhoping to find some troops to hire.");
+	wrefresh(win);
 	getch();
 }
 
 void action_draft_letter()
 {
-	clear();
-	printw("You send messengers to the local taverns,\nhoping to find some troops to hire.");
-	refresh();
+	wclear(win);
+	wprintw(win, "You send messengers to the local taverns,\nhoping to find some troops to hire.");
+	wrefresh(win);
 	getch();
 }
 
 void action_view_inventory()
 {
-	clear();
-	printw("Notable trinkets:\n\nYour father's sword\nGold engraved locket");
-	refresh();
+	wclear(win);
+	wprintw(win, "Notable trinkets:\n\nYour father's sword\nGold engraved locket");
+	wrefresh(win);
 	getch();
 }
 
 void action_view_relations()
 {
-	clear();
-	printw("Relations:\n\n%s\n%s\n\n%s\n%s, %s\n\n%s\n%s\n\n%s\n%s\n\n%s\n%s\n",
-		zander.noble, zander.location,
-		talis.noble, talis.location, grelin.location,
-		adriin.noble, adriin.location,
-		doxoun.noble, doxoun.location,
-		calia.noble, calia.location
-	);
-	refresh();
+	wclear(win);
+	wprintw(win, "Relations:\n\n");
+	for (size_t i = 0; i < allcities.size; ++i) {
+		wprintw(win, "%s\n%s\n\n", allcities.cities[i].noble, allcities.cities[i].location);	
+	}; 
+	wrefresh(win);
 	getch();
 }
 
 void action_view_quests()
 {
-	clear();
-	printw("Quests:\n\nDeliver letter to %s\n- For %s", zander.noble, adriin.noble);
-	refresh();
+	wclear(win);
+	wprintw(win, "Quests:\n\nDeliver letter to %s\n- For %s", zander.noble, adriin.noble);
+	wrefresh(win);
 	getch();
 }
