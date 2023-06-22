fc4cb7fa3a79f364763b1e2d14191bab1cc3cfff
emzilia <giblinhm@miamioh.edu>
2023-06-22 13:08:22 -0400

    now tracking name/upkeep cost of party troops in struct

--- entities.h
+++ entities.h
@@ -34,18 +34,25 @@ typedef struct {
 	Buddy buddies[6];
 	size_t size;
 } BuddyList;
-
 typedef struct{
 	size_t maa;
+	char maaname[20];
+	size_t maaupkeep;
 	size_t pspear;
+	char pspearname[20];
+	size_t pspearupkeep;
 	size_t pbow;
+	char pbowname[20];
+	size_t pbowupkeep;
 	size_t total;
+	size_t totalupkeep;
 } PartyList;
 
 typedef struct {
 	char name[7];
 	char location[20];
 	char noble[20];
+	int iscity;
 	size_t pop;
 	size_t danger;
 	size_t wealth;
