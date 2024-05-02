cfba92bd6e9ba3a5b06e7521925507551fd7f4f3
emzilia <bobby@tables.com>
2024-05-02 02:00:31 -0400

    new file to hold all game text

---
+++ src/text.c
@@ -0,0 +1,20 @@
+
+// Display
+char map[8][28] = {
+	"o==========================o",
+	"|.....c./^^^^^^^^^^^^^^^t..|",
+	"|....../........v^^^^^^^...|",
+	"|.t___/....C.............c.|",
+	"|./............v...........|",
+	"|/.....**t*******..t.......|",
+	"|..t..************......t..|",
+	"o==========================o"
+};
+char titlescreen[200] = "    Hello and welcome to Tania!\n\n"
+	"    The Duke has been gone for three years now,\n"
+	"fighting heathens abroad. Unrest grows across the\n"
+	"realm as local nobles grow to fill the void left\n"
+	"in his absence.";
+
+// Actions
+	
---
+++ src/text.h
@@ -0,0 +1,10 @@
+#ifndef TEXT_H
+#define TEXT_H
+
+// Display
+extern char map[8][28];
+extern char titlescreen[200];
+
+// Actions
+
+#endif
