950e243a4d557ce90b29fd3b42a979664745a2d2
emzilia <giblinhm@miamioh.edu>
2024-05-06 20:18:47 -0400

    storing event structs here

--- src/text.c
+++ src/text.c
@@ -1,3 +1,5 @@
+#include "display.h"
+#include "entities.h"
 
 // Display
 char map[8][28] = {
@@ -10,6 +12,7 @@ char map[8][28] = {
 	"|..t..************......t..|",
 	"o==========================o"
 };
+
 char titlescreen[200] = "    Hello and welcome to Tania!\n\n"
 	"    The Duke has been gone for three years now,\n"
 	"fighting heathens abroad. Unrest grows across the\n"
@@ -17,4 +20,20 @@ char titlescreen[200] = "    Hello and welcome to Tania!\n\n"
 	"in his absence.";
 
 // Actions
-	
+Event1 event_city1 = {
+	.text = "You approach the gates to the %s\n\n"
+		"The guard ushers your party through without\n"
+		"complaint.",
+	.arg = p.location
+};
+
+Event2 event_city2 = {
+	.response = 4,
+	.text = "%s\n\n%s\n\n"
+			"Who would you like to see?\n\n"
+			"1. Guild Master\n2. Tailor\n3. Blacksmith\n4. Stablekeeper"
+			"\n\n\n(b) to go back",
+	.arg1 = p.location,
+	.arg2 = "                                                           "
+			"                                                           "
+};
--- src/text.h
+++ src/text.h
@@ -6,5 +6,7 @@ extern char map[8][28];
 extern char titlescreen[200];
 
 // Actions
+extern Event1 event_city1;
+extern Event2 event_city2;
 
 #endif
